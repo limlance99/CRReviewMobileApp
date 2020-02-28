99722714bd85c828d308959461de45ca0e7a3826
limlance99 <limlance_99@yahoo.com>
2020-02-28 09:49:30 +0800

    added docu

--- adminPage/src/router/index.js
+++ adminPage/src/router/index.js
@@ -1,3 +1,15 @@
+/*
+Authors: JV Afable, JP Chanchico, Lance Lim
+
+This is a course requirement for CS 192 Software Engineering II
+under the supervision of Asst. Prof. Ma. Rowena C. Solamo of
+the Department of Computer Science, College of Engineering,
+University of the Philippines, Diliman for the AY 2019-2020.
+
+Code History:
+	Feb 27, 2020: Lance Lim - Initialized file.
+*/
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
